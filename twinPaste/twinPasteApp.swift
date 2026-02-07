aafe59b76d4a7f183dc4fd8802c2def198d41b19
RP Labs <littlerom@live.fr>
2026-02-07 21:18:09 +0100

    Initial commit - twinPaste clean base

---
+++ twinPaste/twinPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  twinPasteApp.swift
+//  twinPaste
+//
+//  Created by Romain on 07/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct twinPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
